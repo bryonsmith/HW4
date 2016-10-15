2848536f77487554cca76cacb28c1d5665e518eb
Bryon Smith <bryon-smith@uiowa.edu>
2016-10-14 20:36:53 -0500

    created getters and setters and toString

--- junkFoodApp/src/java/model/JunkFood.java
+++ junkFoodApp/src/java/model/JunkFood.java
@@ -24,6 +24,52 @@ public class JunkFood {
         this.calories = calories;
         this.junkFoodRank = junkFoodRank;
     }
+
+    public int getJunkFoodID() {
+        return junkFoodID;
+    }
+
+    public void setJunkFoodID(int junkFoodID) {
+        this.junkFoodID = junkFoodID;
+    }
+
+    public String getJunkFoodName() {
+        return junkFoodName;
+    }
+
+    public void setJunkFoodName(String junkFoodName) {
+        this.junkFoodName = junkFoodName;
+    }
+
+    public String getJunkFoodType() {
+        return junkFoodType;
+    }
+
+    public void setJunkFoodType(String junkFoodType) {
+        this.junkFoodType = junkFoodType;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public int getJunkFoodRank() {
+        return junkFoodRank;
+    }
+
+    public void setJunkFoodRank(int junkFoodRank) {
+        this.junkFoodRank = junkFoodRank;
+    }
+
+    @Override
+    public String toString() {
+        return "JunkFood{" + "junkFoodID=" + junkFoodID + ", junkFoodName=" + junkFoodName + ", junkFoodType=" + junkFoodType + ", calories=" + calories + ", junkFoodRank=" + junkFoodRank + '}';
+    }
+    
     
     
 }
